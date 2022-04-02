c87947a215411763c6bf73bd3b0f7ebb97fb5970
francisco-solis99 <franciscosoliscss@gmail.com>
2022-04-02 16:34:10 -0600

    [Add] some methods and upstaes to creathe the personalized cards

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,71 @@
+<!-- JS -->
 <script>
-import PresentationCard from './components/PresentationCard.vue';
+  import Cards from './components/Cards.vue';
+  import AddCard from './components/AddCard.vue';
   export default {
     name: 'App',
     components: {
-      PresentationCard
+      Cards, AddCard
     },
 
     data(){
       return {
-
+        cardsList: [
+          {
+            id: 0,
+            name: 'Juan',
+            job: 'Developer',
+            palette: 'lofi',
+          },
+          {
+            id: 1,
+            name: 'Richard',
+            job: 'Backend Developer',
+            palette: 'cold',
+          },
+          {
+            id: 2,
+            name: 'Francisco',
+            job: 'Developer',
+            palette: 'warm',
+          },
+        ],
+        copyCards: [],
       }
     },
 
     methods: {
-      closeModal(){
-          const modal = document.querySelector('dialog');
-          modal.close()
-      }
+      addCard(card){
+        this.cardsList.push(card);
+        this.copyCards = [...this.cardsList];
+      },
     },
 
-    mounted() {
-      const dialog = document.querySelector('dialog');
-      const show = document.querySelector('#show');
-      show.addEventListener('click', () => dialog.showModal());
+    created(){
+      this.copyCards = [...this.cardsList];
     },
   }
 </script>
 
+
+<!-- HTML -->
 <template v-cloak>
   <header>
     <h1 class="app__title">Presentation Cards </h1>
   </header>
   <section class="cards">
-    <PresentationCard name="Juan" job="Digital Creator" paletteName="lofi"></PresentationCard>
-    <PresentationCard name="Richard" job="Backend Dev" paletteName="cold"></PresentationCard>
-    <PresentationCard name="Francisco" job="Frontend Dev" paletteName="warm"></PresentationCard>
+    <Cards :cardsList="copyCards"/>
   </section>
-  <button id="show" class="add__btn">
-  </button>
+  <AddCard v-on:create-card="addCard" :cardsList="copyCards"></AddCard>
 </template>
 
+
+<!-- CSS -->
 <style>
   body {
     margin: 0;
     padding: 0;
     font-family: monospace;
-  }
-
-  .app__title {
-    margin-bottom: 30px;
-    text-align: center;
-    font-size: 2rem;
-    text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.25);
-    color: rgb(153, 42, 180);
   }
 
   .cards {
@@ -64,41 +77,12 @@ import PresentationCard from './components/PresentationCard.vue';
     justify-items: center;
   }
 
-  .add__btn {
-    width: 50px;
-    height: 50px;
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    border-radius: 50%;
-    border: 0;
-    background: rgb(153, 42, 180);
-    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.25);
-    cursor: pointer;
-  }
-
-  .add__btn::before, .add__btn::after {
-    content: '';
-    display: block;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-    background: rgb(255, 255, 255);
-  }
-
-  .add__btn::before{
-    width: 5px;
-    height: 60%;
-  }
-
-  .add__btn::after{
-    width: 60%;
-    height: 5px;
-  }
-
-  .add__btn:active {
-    transform: scale(1.1);
+  .app__title {
+    margin-bottom: 30px;
+    text-align: center;
+    font-size: 2rem;
+    text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.25);
+    color: rgb(153, 42, 180);
   }
 
 </style>
